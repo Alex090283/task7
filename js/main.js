d7fb92c8cc18c2e3f18c9e38e1f4e03b86403102
Alex <alex.dev090283@gmail.com>
2020-04-05 09:53:17 +0300

    more mod

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ let
     income1 = document.querySelector('#income'),
     checkSavings = document.querySelector('#savings'),
     sumValue = document.querySelector('#sum'),
-    percent = document.querySelector('#percent'),
+    percentValue = document.querySelector('#percent'),
     yearValue = document.querySelector('.year-value'),
     monthValue = document.querySelector('.month-value'),
     dayValue = document.querySelector('.day-value');
@@ -72,6 +72,63 @@ btnOptionalExpenses.addEventListener('click', function() {
 
 btnCount.addEventListener('click', function() {
 
+if (appData.budget !=undefined) {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    daybudgetValue.textContent = appData.moneyPerDay;
+
+    if (appData.moneyPerDay < 100) {
+        levelValue.textContent = "Минимальный уровень достатка!";
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        levelValue.textContent = "Средний уровень достатка!";
+    } else if (appData.moneyPerDay > 2000) {
+        levelValue.textContent = "Высокий уровень достатка!";
+    } else {
+        levelValue.textContent = "Произошла ошибка!";
+    }
+} else {
+    daybudgetValue.textContent = "Произошла ошибка!";
+}
+
+});
+
+income1.addEventListener('input', function() {
+    let items = income1.value;
+    appData.income = items.split(', ');
+    incomeValue.textContent = appData.income;
+});
+
+checkSavings.addEventListener('click', function() {
+    if (appData.savings == true) {
+        appData.savings = false;
+    } else {
+        appData.savings = true;
+    }
+});
+
+sumValue.addEventListener('input', function() {
+    if (appData.savings == true) {
+        let sum = +sumValue.value,
+            percent = +percentValue.value;
+
+        appData.monthIncome = sum/100/12*percent;
+        appData.yearIncome = sum/100*percent;
+
+        monthsavingsValue.textContent = appData.monthIncome.toFixed(1);
+        yearsavingsValue.textContent = appData.yearIncome.toFixed(1);
+    }
+});
+
+percentValue.addEventListener('input', function() {
+    if (appData.savings == true) {
+        let sum = +sumValue.value,
+            percent = +percentValue.value;
+
+        appData.monthIncome = sum/100/12*percent;
+        appData.yearIncome = sum/100*percent;
+
+        monthsavingsValue.textContent = appData.monthIncome.toFixed(1);
+        yearsavingsValue.textContent = appData.yearIncome.toFixed(1);    
+    }
 });
 
 
@@ -81,44 +138,10 @@ let appData = {
     optionalExpenses: {},
     income: [],
     timeData: time,
-    savings: true,
-    chooseExpenses: function() {
-        
-    },
-    detectDayBuget: function () {
-        appData.moneyPerDay = (appData.budget / 30).toFixed();
-        alert("Ежедневный бюджет: " + appData.moneyPerDay);
-    },
-    detectLevel: function() {
-        if (appData.moneyPerDay < 100) {
-            console.log("Минимальный уровень достатка");
-        } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-            console.log("Средний уровень достатка");
-        } else if (appData.moneyPerDay > 2000) {
-            console.log("Высокий уровень достатка");
-        } else {
-            console.log("Произошла ошибка!");
-        }
-    },
-    checkSavings: function() {
-        if (appData.savings == true) {
-            let save = +prompt("Какова сумма накоплений?"),
-                percent = +prompt("Под какой процент?");
+    savings: false,
+   
+};   
     
-            appData.monthIncome = save/100/12*percent;
-            alert("Доход в месяц с вашего депозита: " + appData.monthIncome);    
-        }
-    },
-    chooseOptExpenses: function() {
-        
-    },
-    chooseIncome: function() {
-        let items = prompt("Что принесет дополнительный доход? (Перечислите через запятую)", "");
-        appData.income = items.split(', ');
-        appData.income.push(prompt("Может быть что-то еще?"));
-        appData.income.sort();
-    }
-};
 
 
 
